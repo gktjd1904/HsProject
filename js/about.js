ece8464b1186cdbe7d97d4eb30b3f1f799211cd6
gktjd1904 <80248494+gktjd1904@users.noreply.github.com>
2022-12-06 14:24:31 +0900

    111

---
+++ js/about.js
@@ -0,0 +1,11 @@
+/* let scr = document.querySelector('body');
+
+window.addEventListener('scroll', function() {
+    let value = window.scrollY
+    console.log("scrolly",value);
+
+    if (value > 900) {
+        scr.style = 'background-color : red'
+    }});
+
+ */
